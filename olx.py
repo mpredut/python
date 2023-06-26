44a20ec4ae3c38fa8824b67ed7d5c4d13b96e82c
marius predut <marius@dotlumen.com>
2023-06-26 11:52:28 +0300

    fixes + updates

--- olx.py
+++ olx.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from re import search
 
 
+yearreference = 2006 - 1
 url = "https://www.olx.ro/auto-masini-moto-ambarcatiuni/autoturisme/<BREND>/?currency=EUR&page=<PAGE>&search%5Bfilter_enum_model%5D%5B0%5D=<MODEL>"
 
 
@@ -39,10 +40,14 @@ def only_numerics(seq):
  
  
 
-def parseurl(url, text): 
+def parseurl(url, text, minyear, maxprice): 
     
+    global yearreference
+    
+    ratereference = maxprice / (minyear - yearreference)
     text = text.lower()
-    #print("[url " + url + " cheie " + text +"]")
+    print("[url " + url + " cheie " + text +"]")
+    #print("rateref " + str(ratereference))
     
     response = requests.get(url)
 
@@ -67,41 +72,51 @@ def parseurl(url, text):
         
         strtitle = str(title.text.strip()).lower();
         
-        strprice = ""
+        strprice = "0"
         if(price is not None) :
             strprice = only_numerics(str(price.text.strip()).lower());
+        if(strprice == "") :
+            strprice = "0";
         
-        text = text.lower()
+        #text = text.lower()
         #if(text != "" and ( not text in strtitle)):
         #    continue
-        
+ 
         year = "1000"
         if(yearandkm is not None and str(yearandkm.text.strip()).find(" - ") != -1):
             split = yearandkm.text.strip().split(" - ")
             year = split[0]
             km = split[1]
 
+        stryear = only_numerics(str(year))
+        
+        
+        if(int(stryear) <= yearreference) :
+            continue;
+
         if(ancora is not None) : 
             #print("Ancora " + ancora['href']);
             strancora = ancora['href'];
         if(not search("www.autovit.ro", strancora)): 
             strancora = "https://www.olx.ro/" + strancora;
+        #print(strancora)
             
-        stryear = only_numerics(str(year))
-
-        if((strtitle.find("leasing") != -1 or strtitle.find("rate") != -1)):
-            if(strprice == "") :
-                find_items  = True
-                #print(f"AN:{stryear} Pret: LIPSA!. {strtitle} ")  
-                data.update([(stryear, 'LIPSA', strtitle, strancora)])
-            else :
-                if (int(strprice) < 41000) :
-                    find_items  = True
-                    #print(f"AN:{stryear} Pret: {strprice}. {strtitle} ") 
-                    data.update([(stryear, strprice, strtitle, strancora)])
+        rate = int(strprice) / (int(stryear) - yearreference)
+        #print("rate " + str(rate))
+        if(rate > ratereference) :
+            #print("ratereference" + str(ratereference))
+            #print(strancora)
+            continue;
         
+        if((strtitle.find("leasing") != -1 or strtitle.find("rate") != -1)):
+            if (int(strprice) < 41000) :
+                find_items  = True
+                #print(f"AN:{stryear} Pret: {strprice}. {strtitle} ") 
+                data.update([(stryear, strprice, strtitle, strancora)])
+                    
+
         if (stryear != "" and strprice != "") :
-            if(int(stryear) > 2008 and int(strprice) < 10000) :
+            if(int(stryear) > 2008 and int(strprice) < 14000) :
                 find_items  = True
                 #print("AN:" + stryear + " Pret: " +  strprice +  ". " + strtitle)   
                 data.update([(stryear, strprice, strtitle, strancora)])
@@ -123,7 +138,7 @@ def parseurl(url, text):
 
 #cautamasina("marca", "model")
 
-def cautamasina(brend, model): 
+def cautamasina(brend, model, minyear, maxprice): 
     print("")
 
     global url
@@ -135,7 +150,8 @@ def cautamasina(brend, model):
     response = requests.get(urlsearch)
     content = response.content
     soup = BeautifulSoup(content, 'html.parser')
-    nb = soup.find('div', {'data-testid': 'total-count'})
+    #print(soup)
+    nb = soup.find('span', {'data-testid': 'total-count'})
     #print(nb)
     if(nb is None) :
        print("nu exista pagina " + urlsearch)
@@ -147,9 +163,9 @@ def cautamasina(brend, model):
     nb = int(int(strnb) / 32) + 1
     print(strnb + " anunturi in " + str(nb) + " pagini pentru " + model)
     
-    for i in range(1, nb) :
+    for i in range(0, nb) :
         urlsearch = urlreal.replace("<PAGE>", str(i))
-        parseurl(urlsearch, model)
+        parseurl(urlsearch, model, minyear, maxprice)
        
     global data       
     datas = sorted(data)
@@ -159,13 +175,25 @@ def cautamasina(brend, model):
 
 #MAIN
 
-brend = "lexus"
-model = "rx"
 
-cautamasina("ford", "explorer")
-cautamasina("lexus", "seria-rx")
-cautamasina("lexus", "egyeb") #altul
-#cautamasina("toyota", "land-cruiser")
-#cautamasina("bmw", "x5")
-#cautamasina("nissan", "navara")
-#cautamasina("toyota", "hilux")
+#Anul de referinta este setat la 2005. vei cauta incepand cu 2006
+
+cautamasina("maserati", "", 2008, 8000)
+cautamasina("ford", "explorer", 2008, 8000)
+cautamasina("ford", "mustang", 2008, 6000)
+cautamasina("lexus", "seria-rx", 2008, 8000)
+cautamasina("lexus", "seria-lx", 2008, 8000)
+cautamasina("lexus", "egyeb", 2008, 8000) #altul
+cautamasina("toyota", "land-cruiser", 2008, 8000)
+#cautamasina("bmw", "x5", 2010, 8000)
+#cautamasina("volvo", "xc-90", 2010, 8000)
+#cautamasina("volvo", "xc-60", 2010, 6000)
+cautamasina("nissan", "navara", 2008, 4500)
+cautamasina("toyota", "hilux", 2008, 7000)
+cautamasina("toyota", "highlander", 2008, 7000)
+#cautamasina("land-rover","", 2008, 5500)
+#cautamasina("mercedes-benz","gle", 2008, 7000)
+#cautamasina("mercedes-benz","gls", 2008, 8000)
+#cautamasina("mercedes-benz","gl-class", 2008, 7000)
+
+
